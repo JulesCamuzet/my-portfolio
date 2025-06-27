aef7c30c19f1b19863c7338d6efd46818496b6d2
Jules Camuzet <jules.camuzet@yetic.org>
2025-06-27 14:14:00 +0200

    feat(skills): update pictures

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -4,22 +4,22 @@ const skillsCards: SkillsData = [
   {
     id: 'typescript',
     name: 'Typescript',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png',
+    img: `${process.env.API_URL}/images/typescript.png`,
   },
   {
     id: 'react',
     name: 'React',
-    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/862px-React-icon.svg.png',
+    img: `${process.env.API_URL}/images/react.png`,
   },
   {
     id: 'node',
     name: 'NodeJS',
-    img: 'https://img.icons8.com/fluent/512/node-js.png',
+    img: `${process.env.API_URL}/images/node-js.png`,
   },
   {
     id: 'git',
     name: 'Git',
-    img: 'https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png',
+    img: `${process.env.API_URL}/images/git.png`,
   },
 ]
 
