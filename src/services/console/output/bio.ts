46e335de467f58cd6747d77176e9f74a0b149a55
Jules Camuzet <jules.camuzet@yetic.org>
2025-06-30 12:00:10 +0200

    fix(bio): update bio

--- src/services/console/output/bio.ts
+++ src/services/console/output/bio.ts
@@ -8,11 +8,11 @@ export const outputBio = (input: string) => {
       meet real-world needs. What drives me is the opportunity to build
       useful, well-crafted applications that have a positive impact.
       <br />
-      I’m currently working as a freelance developer, taking on projects
-      that allow me to strengthen my skills and collaborate with others.
-      At the same time, I’m looking for a work-study opportunity, where I
-      can continue learning and contribute to meaningful and innovative
-      projects.
+      I’m currently working in a startup as part of a work-study program,
+      where I contribute to real projects and continue developing my skills
+      in a dynamic environment. In addition to that, I also work as a
+      freelance developer, taking on various projects that help me grow
+      professionally and collaborate with different clients.
     `
 
   return output
